31986abb10b4c5ca0e59a2005b7dbf146bdd2147
Juan Antonio Cortés <jacortesm@gmail.com>
2021-07-29 08:48:42 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-let izquierda = 0
 let derecha = 0
+let izquierda = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     izquierda = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
     derecha = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
@@ -16,5 +17,27 @@ basic.forever(function () {
                 maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
             }
         }
+    }
+})
+control.inBackground(function () {
+    for (let index = 0; index < 1000000000000000; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Violet))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+        basic.pause(500)
     }
 })
